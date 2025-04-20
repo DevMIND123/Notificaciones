3f162b847db1317fad491091182ed9891da101a3
Johan Nicolas Moyano Poveda <jnicolas_moyano@javeriana.edu.co>
2025-04-20 16:04:54 -0500

    Modulo de notificaciones v1.0.0 (#2)
    * Se implementa una primera notificación al momento de registrarse
    
    * Se agrego el rol del usuario en la notificación
    
    * Arreglando conexion con front
    
    ---------
    
    Co-authored-by: Luisa vargas <luchisvargasgomez@gmail.com>
    Co-authored-by: maikolUniversidad <maikol.vergara@javeriana.edu.co>

---
+++ NotificacionesService/Migrations/20250415003543_AgregarTipoUsuario.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NotificacionesService.Migrations
+{
+    /// <inheritdoc />
+    public partial class AgregarTipoUsuario : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TipoUsuario",
+                table: "notificaciones",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TipoUsuario",
+                table: "notificaciones");
+        }
+    }
+}
